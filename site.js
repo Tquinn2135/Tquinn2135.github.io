41bab59d37b968b57b01aac8d8b5a9d38c138e59
Tom Quinn <Tquinn2135@gmail.com>
2025-09-22 14:12:06 -0500

    Added secret message!

--- site.js
+++ site.js
@@ -1,6 +1,7 @@
 
 const hours = new Date().getHours() // get the current hour
-
+const key = 'It\'s a secret to everybody.'
+const message = 'It\s dangerous to go alone take this!'
 const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
@@ -22,7 +23,7 @@ const isEvening = hours >= 17 || hours < 4 // is it evening?
     welcome.innerHTML = '<p>Good Evening</p>'
  }
  
-
+const secretMessage = localStorage.setItem(key, message) 
  
  
 console.log("Hi")
