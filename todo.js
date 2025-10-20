b0ed27c1bf355130f01c62f90f32d4af78bf0253
TQuinn2135 <Tquinn2135@gmail.com>
2025-10-20 01:30:51 -0500

    Clean up

--- todo.js
+++ todo.js
@@ -1,26 +1,26 @@
-const todos = JSON.parse(localStorage.getItem('todos')) || [];
-const addButton = document.getElementById('addButton');
-const newTodoInput = document.getElementById('new-todo');
-const todoList = document.querySelector('.todo-list');
+const todos = JSON.parse(localStorage.getItem('todos')) || []
+const addButton = document.getElementById('addButton')
+const newTodoInput = document.getElementById('new-todo')
+const todoList = document.querySelector('.todo-list')
 
 
 const renderTodos = () => {
-    todoList.innerHTML = '';
+    todoList.innerHTML = ''
     todos.forEach(todo => {
-        const li = document.createElement('li');
-        li.textContent = todo.Text;
-        todoList.append(li);
-    });
-};
-renderTodos();
+        const li = document.createElement('li')
+        li.textContent = todo.Text
+        todoList.append(li)
+    })
+}
+renderTodos()
 
 addButton.addEventListener('click', () => {
-    const newTodoText = newTodoInput.value.trim();
+    const newTodoText = newTodoInput.value
     if (newTodoText !== '') {
-        const newTodo = { Text: newTodoText, Completed: false };
-        todos.push(newTodo);
-        localStorage.setItem('todos', JSON.stringify(todos));
-        renderTodos();
-        newTodoInput.value = '';
+        const newTodo = { Text: newTodoText, Completed: false }
+        todos.push(newTodo)
+        localStorage.setItem('todos', JSON.stringify(todos))
+        renderTodos()
+        newTodoInput.value = ''
     }
-});
+})
